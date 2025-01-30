72a1ae336b07c2b6e37d34dc2bbef3b4a92ac5a6
kartik120-pk <118261121+kartik120-pk@users.noreply.github.com>
2025-01-30 14:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
-}
+  s = s.trim();
 
+  // If the string contains a space, return the part before the first space
+  if (s.includes(' ')) {
+    return s.split(' ')[0];
+  } else {
+    // If there’s no space, return the whole string
+    return s;
+  }
+
+}
 // Do not change the code below
 
-const s = prompt("Enter String:");
+ const s = prompt("Enter String:");
 alert(firstWord(s));
